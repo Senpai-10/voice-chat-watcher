c136133c00083cd18ec83e75b09bf465427b546f
abdalrzag eisa <bmjfdrh@gmail.com>
2022-06-06 10:14:00 +0300

    move config.ts to index.ts and ask for info if not found

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,62 @@
 import { Client } from "discord.ts-selfbot";
-import config from "./config";
 import notifier from "node-notifier";
+import editjsonfile from "edit-json-file";
+import inquirer from "inquirer";
 import { panic } from "./util/panic";
 
 // Main function
-function main() {
-    if (config.channels_list.length == 0) {
+async function main() {
+    const is_dev = (): boolean =>
+        Number(process.env.dev) == 1 || Boolean(process.env.dev) == true;
+
+    let config_path: string = is_dev() ? "./.config.dev.json" : "./config.json";
+
+    const config = editjsonfile(config_path, {
+        autosave: true,
+        stringify_width: 4,
+    });
+
+    let discord_token: string = config.get("discord_token");
+    let channels_list: Array<string> = config.get("channels_list");
+
+    if (!discord_token) {
+        let { token } = await inquirer.prompt({
+            name: "token",
+            message: "Enter you discord token",
+            type: "input",
+        });
+
+        discord_token = token;
+        config.set("discord_token", token);
+    }
+
+    if (channels_list.length == 0) {
+        let { channelID } = await inquirer.prompt({
+            name: "channelID",
+            message: "Enter channel ID",
+            type: "number",
+        });
+
+        channels_list.push(channelID);
+        config.set("channels_list", channels_list);
+    }
+
+    if (channels_list.length == 0) {
         panic(
             `'channels_list' can't be empty\nplease add ids to 'channels_list' (in config.json)`
         );
     }
 
     const client = new Client();
-    client.login(config.discord_token);
+    client.login(discord_token);
 
     client.once("ready", () => {
-        console.log(`Watching ${config.channels_list.length} channels.`);
+        console.log(`Watching ${channels_list.length} channels.`);
     });
 
-    client.on("voiceStateUpdate", (oldChannel, newChannel) => {
+    client.on("voiceStateUpdate", (_, newChannel) => {
         if (
-            config.channels_list.includes(newChannel.channelID) == false ||
+            channels_list.includes(newChannel.channelID) == false ||
             newChannel.channel.members.size != 1
         )
             return;
